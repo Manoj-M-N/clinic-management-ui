2ff7aaecd2b91315a31b5075d5d92046b833faca
Manoj M N <manojmarutheshn@gmail.com>
2026-05-20 12:55:50 +0530

    fix url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://clinic-backend.onrender.com",
+  baseURL: "https://clinic-backend-yw16.onrender.com",
 });
 
 export default api;
